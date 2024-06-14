6b4258e16f8d8987a14a43cee40d9d8fc3c334de
Wilgen Garcia Rodriguez <wilgengarcia2001@gmail.com>
2024-06-14 15:30:58 -0500

    chore: :heavy_plus_sign: agrega dependencia react-bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './scss/custom.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import './scss/custom.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
